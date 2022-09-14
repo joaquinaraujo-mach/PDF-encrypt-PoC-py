f026d8e6c7bd85bc1265daf17e268f9b2142166c
Jorge Silva <jorge.silval@bci.cl>
2022-09-13 22:17:43 -0300

    Lambda function ready

--- src/run.py
+++ src/run.py
@@ -1,14 +1,15 @@
 from helpers.encrypt_pdf import EncryptPDF
 from helpers.S3 import S3
+from helpers.utils import Utils
 
 # Import Libraries
 from io import BytesIO
 from datetime import datetime
 
-S3_BUCKET_NAME = 'encrypt-pdf'
-INPUT_FILE = 'without_password.pdf'
-PASSWORD = '123asd'
-OUTPUT_FILE = 'encrypted'
+S3_BUCKET_NAME = 'no-encrypted-pdf'
+S3_DESTINATION_BUCKET_NAME = 'encrypt-pdf'
+INPUT_FILE = 'without_password_d_19194798_asd.pdf'
+OUTPUT_FILE = 'without_password_d_19194798_asd'
 
 if __name__ == '__main__':
     #Get file from s3
@@ -18,7 +19,7 @@ if __name__ == '__main__':
         fs = file_object['Body'].read()
         #Encrypt PDF
         result, pdf_reader, pdf_writer = EncryptPDF.encrypt_pdf(
-                input_file=BytesIO(fs), password=PASSWORD)
+                input_file=BytesIO(fs), password=Utils.generate_password(INPUT_FILE))
 
         # Encryption completed successfully
         if result:
@@ -26,5 +27,5 @@ if __name__ == '__main__':
             with BytesIO() as bytes_stream:
                 pdf_writer.write(bytes_stream)
                 bytes_stream.seek(0)
-                S3.upload(S3_BUCKET_NAME, bytes_stream, f'{OUTPUT_FILE}_{datetime.now().isoformat()}.pdf', 'application/pdf')
+                S3.upload(S3_DESTINATION_BUCKET_NAME, bytes_stream, f'{OUTPUT_FILE}_encrypted.pdf', 'application/pdf')
         
